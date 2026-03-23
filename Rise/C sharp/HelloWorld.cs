dddc0eef2a1cb909592cc0d8286e2963fb5b6d25
SHIV BHAVESH PATEL <pshiv8753@gmail.com>
2026-03-23 17:42:50 +0530

    Add files via upload

---
+++ Rise/C sharp/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstCSharpApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello SHIV BHAVESH PATEL!");
+            Console.WriteLine("My first C# program");
+        }
+    }
+}
